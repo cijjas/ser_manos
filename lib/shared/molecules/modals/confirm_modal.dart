3570c35982500967ff58279d1a83f2e19eae955b
cijjas <cijjas@itba.edu.ar>
2025-04-26 11:43:31 -0300

    fix button size error

--- lib/shared/molecules/modals/confirm_modal.dart
+++ lib/shared/molecules/modals/confirm_modal.dart
@@ -1,76 +1,76 @@
-import 'package:flutter/material.dart';
-import '../../tokens/shadow.dart';
-import '../../tokens/typography.dart';
-import '../../tokens/colors.dart';
-import '../buttons/app_button.dart';
-
-class ConfirmApplicationModal extends StatelessWidget {
-  final String title;
-  final VoidCallback onConfirm;
-  final VoidCallback onCancel;
-
-  const ConfirmApplicationModal({
-    super.key,
-    required this.title,
-    required this.onConfirm,
-    required this.onCancel,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Material( // <--- very important inside a dialog
-        color: Colors.transparent,
-        child: Container(
-          width: 280,
-          padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(4),
-            boxShadow: AppShadows.shadow3,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Te estas por postular a',
-                style: AppTypography.subtitle01.copyWith(
-                  color: AppColors.neutral100,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                title,
-                style: AppTypography.headline02.copyWith(
-                  color: AppColors.neutral100,
-                ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  AppButton(
-                    label: 'Cancelar',
-                    onPressed: onCancel,
-                    type: AppButtonType.tonal,
-                    fillWidth: false,
-                    widthType: AppButtonWidthType.hug,
-                  ),
-                  const SizedBox(width: 8),
-                  AppButton(
-                    label: 'Confirmar',
-                    onPressed: onConfirm,
-                    type: AppButtonType.tonal,
-                    fillWidth: false,
-                    widthType: AppButtonWidthType.hug,
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import '../../tokens/shadow.dart';
+// import '../../tokens/typography.dart';
+// import '../../tokens/colors.dart';
+// import '../buttons/app_button.dart';
+//
+// class ConfirmApplicationModal extends StatelessWidget {
+//   final String title;
+//   final VoidCallback onConfirm;
+//   final VoidCallback onCancel;
+//
+//   const ConfirmApplicationModal({
+//     super.key,
+//     required this.title,
+//     required this.onConfirm,
+//     required this.onCancel,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Material( // <--- very important inside a dialog
+//         color: Colors.transparent,
+//         child: Container(
+//           width: 280,
+//           padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+//           decoration: BoxDecoration(
+//             color: Colors.white,
+//             borderRadius: BorderRadius.circular(4),
+//             boxShadow: AppShadows.shadow3,
+//           ),
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text(
+//                 'Te estas por postular a',
+//                 style: AppTypography.subtitle01.copyWith(
+//                   color: AppColors.neutral100,
+//                 ),
+//               ),
+//               const SizedBox(height: 8),
+//               Text(
+//                 title,
+//                 style: AppTypography.headline02.copyWith(
+//                   color: AppColors.neutral100,
+//                 ),
+//               ),
+//               const SizedBox(height: 16),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.end,
+//                 children: [
+//                   AppButton(
+//                     label: 'Cancelar',
+//                     onPressed: onCancel,
+//                     type: AppButtonType.tonal,
+//                     fillWidth: false,
+//                     widthType: AppButtonWidthType.hug,
+//                   ),
+//                   const SizedBox(width: 8),
+//                   AppButton(
+//                     label: 'Confirmar',
+//                     onPressed: onConfirm,
+//                     type: AppButtonType.tonal,
+//                     fillWidth: false,
+//                     widthType: AppButtonWidthType.hug,
+//                   ),
+//                 ],
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
