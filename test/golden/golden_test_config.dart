cacb8af1e435b0e65080edbb0dd178918cc1bbe2
meursault00 <akibengolea@icloud.com>
2025-06-15 14:25:30 -0300

    Add first golden tests

---
+++ test/golden/golden_test_config.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  await loadAppFonts();
+  return GoldenToolkit.runWithConfiguration(
+        () async => await testMain(),
+    config: GoldenToolkitConfiguration(
+      // skipGoldenAssertion se omite: usa el default (none)
+      fileNameFactory: (name) => 'goldens/$name.png',
+    ),
+  );
+}
