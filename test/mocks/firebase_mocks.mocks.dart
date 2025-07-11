f2e7d3e8fce5bdaca3646cffe147d653dc2328d1
meursault00 <akibengolea@icloud.com>
2025-07-11 19:06:36 -0300

    fix eof issue

--- test/mocks/firebase_mocks.mocks.dart
+++ test/mocks/firebase_mocks.mocks.dart
@@ -2440,6 +2440,7 @@ class MockFirebaseFirestore extends _i1.Mock implements _i6.FirebaseFirestore {
 /// A class which mocks [CollectionReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
+// ignore: must_be_immutable
 class MockCollectionReference<T extends Object?> extends _i1.Mock
     implements _i6.CollectionReference<T> {
   MockCollectionReference() {
@@ -2932,6 +2933,7 @@ class MockCollectionReference<T extends Object?> extends _i1.Mock
 /// A class which mocks [DocumentReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
+// ignore: must_be_immutable
 class MockDocumentReference<T extends Object?> extends _i1.Mock
     implements _i6.DocumentReference<T> {
   MockDocumentReference() {
